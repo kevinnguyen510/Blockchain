b4d8daecd2976c431f4298f47c2163371448e16d
Kevin <kevinnguyen510@gmail.com>
2022-05-05 17:31:07 +0700

    init project
    create blockchain

---
+++ Module1_Create_Block_Chain/blockchain.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  5 17:01:43 2022
+
+@author: Trung Nguyen
+"""
+
+
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+#Part 1 - Building a Blockchain
+class Blockchain:
+    
